d7e29000f95161bb4a95402ad3eecc4a6f153b8b
LuanChagas <luanchagas@hotmail.com>
2024-05-04 18:06:38 -0300

    repo criado

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'roboto': ['Roboto Mono', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
